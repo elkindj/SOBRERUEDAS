4ca707b46f91088ebd70f8e430f573d3b03ae4c5
kmaltetecnoevolucion <kmalte@tecnoevolucion.com>
2021-03-03 09:54:33 -0500

    nuevos cambios

---
+++ TallerMecanico.Entidades/Productos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerMecanico.Entidades
+{
+    public class Productos
+    {
+        int _id;
+        String _codigo;
+        int _modelo;
+        String _nombre;
+        String _detalles;
+        int _precio;
+
+        public int Id {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public String Codigo {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+        public int Modelo {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+        public String Nombre {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public String Detalles {
+            get { return _detalles; }
+            set { _detalles = value; }
+        }
+        public int Precio {
+            get { return _precio; }
+            set { _precio = value; }
+        }
+        public Productos(int Id, string Codigo, int Modelo, string Nombre,string Detalles,int Precio)
+        {
+            this._id = Id;
+            this._codigo = Codigo;
+            this._modelo = Modelo;
+            this._nombre = Nombre;
+            this._detalles = Detalles;
+            this._precio = Precio;
+        }
+        //public Productos(int Id, string Codigo, int Modelo, string Nombre, int Precio) : this(Id, Codigo, Modelo, Nombre,"",Precio)
+        //{
+        //}
+        //public Productos(int Id,int Modelo, string Nombre, int Precio) : this(Id, "",Modelo, Nombre, "",Precio)
+        //{
+        //}
+       // public Productos(int Id, int Modelo, int Precio) : this(Id, "", Modelo, "", Precio)
+        //{
+        //}
+        public Productos() : this(0, "",0, "", "",0)
+        {
+        }
+
+    }
+}
---
+++ TallerMecanico.Negocios/BLProducto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Datos;
+using TallerMecanico.Entidades;
+
+namespace TallerMecanico.Negocios
+{
+    public class BLProducto
+    {
+        public List<Productos> Listar()
+		{
+			DAOProductos daProducto = new DAOProductos();
+			return daProducto.Listar();
+		}
+
+		public Productos ProductoTraerPorId(int Id)
+		{
+			DAOProductos daProducto = new DAOProductos();
+			return daProducto.TraerPorId(Id);
+		}
+		public int Insertar(Productos Productos)
+		{
+			DAOProductos daProducto = new DAOProductos();
+			return daProducto.Insertar(Productos);
+		}
+
+		public int Actualizar(Productos Productos)
+		{
+			DAOProductos daProducto = new DAOProductos();
+			return daProducto.Actualizar(Productos);
+		}
+		public int Eliminar(int id)
+		{
+			DAOProductos daProducto = new DAOProductos();
+			return daProducto.Eliminar(id);
+		}
+	}
+}
